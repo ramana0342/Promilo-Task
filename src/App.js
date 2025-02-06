23872d4696ffc81cc2d05f0cf2a49f6d706a7606
Ramana <ramanareddy.m0342@gmail.com>
2025-02-06 08:02:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PromiloApp from "./PromiloComponents/promiloApp"
+
+function App() {
+  return (
+    <>
+    <PromiloApp/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/PromiloComponents/promiloApp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import DashBoard from './DashBoard'
+import "./PromiloApp.css"
+
+
+
+
+
+function PromiloApp() {
+  return (
+    <>
+   <DashBoard/>
+    </>
+  )
+}
+
+
+export default  PromiloApp
